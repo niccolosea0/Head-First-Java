d4215a1274f0946253fd6bac4082f3ecaa457a15
niccolosea0 <niccolosea@gmail.com>
2025-12-15 16:13:03 +0400

    Modified exercise1a

--- src/main/chapter01/Exercise1a.java
+++ src/main/chapter01/Exercise1a.java
@@ -5,11 +5,13 @@
 
 /*
  * This file compiles, but goes in infinite loop. To exit infinite loop press CTRL+C
+ * to prevent infinite loop, we need to increment x variable
 */
 
 class Exercise1a {
     public static void main(String[] args) {
         int x = 1;
+	x = x + 1;
 	while (x < 10) {
 	    if (x > 3) {
 	        System.out.println("big x");
