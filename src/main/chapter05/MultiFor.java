2c15acc424fc02a18b8c6d480e59828a764ec394
niccolosea0 <niccolosea@gmail.com>
2025-12-25 14:51:48 +0400

    Add chapter05 source code

--- src/main/chapter05/MultiFor.java
+++ src/main/chapter05/MultiFor.java
@@ -1,27 +1,17 @@
-/* 	Exercise Code Magnets
- *
- *  A working Java program is all scrambled up on the fridge. Can you reconstruct the code snippets to make a working Java program that
- *  produces the output listed below? Some of the curly braces fell on the floor and they were too small to pick up, so feel free to add as many of
- *  those as you need!
- *	
- *	Expected Output: 0 4
-			 0 3
-			 1 4
-			 1 3
-			 3 4
-			 3 3
- */
+package ch5;
 
 class MultiFor {
-	public static void main(String[] args) {
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = 4; j > 2; j--) {
-				System.out.println(i + " " + j);
-			}
-			if (i == 1) {
-				i++;
-			}
-		}
-	}
+  public static void main(String[] args) {
+    for (int i = 0; i < 4; i++) {
+
+      for (int j = 4; j > 2; j--) {
+        System.out.println(i + " " + j);
+      }
+
+      if (i == 1) {
+        i++;
+      }
+    }
+  }
 }
