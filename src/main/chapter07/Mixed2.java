1b04d742796ec7e2e2ab85f0ec00b3ac248ebec0
niccolosea0 <niccolosea@gmail.com>
2026-01-06 12:56:11 +0400

    Add Mixed message task, second and third code candidates

--- src/main/chapter07/Mixed2.java
+++ src/main/chapter07/Mixed2.java
@@ -14,28 +14,28 @@ class A {
     int ivar = 7;
 
     void m1() {
-        System.out.println("A's m1, ");
+        System.out.print("A's m1, ");
     }
 
     void m2() {
-        System.out.println("A's m2 ");
+        System.out.print("A's m2, ");
     }
 
     void m3() {
-        System.out.println("A's m3 ");
+        System.out.print("A's m3, ");
     }
 }
 
 class B extends A {
 
     void m1() {
-        System.out.println("B's m1 ");
+        System.out.print("B's m1, ");
     }
 }
 
 class C extends B {
     void m3() {
-        System.out.println("C's m3 " + (ivar + 6));
+        System.out.print("C's m3, " + (ivar + 6));
     }
 
 }
@@ -48,10 +48,31 @@ public class Mixed2 {
         C c = new C();
         A a2 = new C();
 
-        b.m1(); // B's m1
-        c.m2(); // A's m2
-        a.m3(); // A's m3
-         
+        b.m1(); // B's m1,
+        c.m2(); // A's m2,
+        a.m3(); // A's m3,
+
+        System.out.println();
+
+        c.m1(); // B's m1,
+        c.m2(); // A's m2,
+        c.m3(); // C's m3, 13
+
+        System.out.println();
+
+        a.m1(); // A's m1,
+        b.m2(); // A's m2,
+        c.m3(); // C's m3, 13
+
+        System.out.println();
+
+        a2.m1(); // B's m1,
+        a2.m2(); // A's m2,
+        a2.m3(); // C's m3, 13
+
+        System.out.println();
+ 
+
     }
 }
 
