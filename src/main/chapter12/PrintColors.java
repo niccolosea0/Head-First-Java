7ad7f5339d09438b62bd442bebabe3c8189276d6
niccolosea0 <niccolosea@gmail.com>
2026-01-14 17:32:35 +0400

    Add printcolors.java, from chapter12

---
+++ src/main/chapter12/PrintColors.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class PrintColors {
+    public static void main(String[] args) {
+
+        List<String> allColors = List.of("Yellow", "Blue", "Purple", "Navy");
+
+        allColors.forEach(color -> System.out.println(color));
+    }
+}
