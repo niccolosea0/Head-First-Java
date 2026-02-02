85d3ab263d7842c8f07958a0152bd9b329ccd960
niccolosea0 <niccolosea@gmail.com>
2026-02-02 10:34:41 +0400

    Add MyDrawPanel.java

---
+++ src/main/chapter14/MyDrawPanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyDrawPanel extends JPanel {
+    
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.orange);
+
+        g.fillRect(20, 50, 100, 100);
+    }
+
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MyDrawPanel());
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
