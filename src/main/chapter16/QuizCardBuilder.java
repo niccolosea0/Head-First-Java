b76f49102e8bbc86fa061cda6d9117086a754488
niccolosea0 <niccolosea@gmail.com>
2026-02-03 12:34:52 +0400

    Add Quiz Card Builder code outline

---
+++ src/main/chapter16/QuizCardBuilder.java
@@ -0,0 +1,33 @@
+public class QuizCardBuilder {
+
+    public void go() {
+
+        // build and display the gui
+    }
+
+    private void nextCard() {
+
+        // add the current card to the list
+        // and clear the text areas
+    }
+
+    private void saveCard() {
+       
+        // bring up a file dialog box
+        // let the user name and save the set
+    }
+
+    private void clearCard() {
+
+        // clear out the text areas
+    }
+
+    private void saveFile(File file) {
+
+        // iterate through the list of cards and write
+        // each one out to a text file in a parseable way
+        // (in other words, with clear separations between parts)
+    }
+}
+
+
