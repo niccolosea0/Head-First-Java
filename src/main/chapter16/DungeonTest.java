e4cec3e04941f30e8f5d6ee2acebe869307dd587
niccolosea0 <niccolosea@gmail.com>
2026-02-03 17:26:35 +0400

    Add Code Magnets

---
+++ src/main/chapter16/DungeonTest.java
@@ -0,0 +1,54 @@
+import java.io.*;
+
+public class DungeonTest {
+    public static void main(String[] args) {
+
+        DungeonGame d = new DungeonGame();
+        System.out.println("Before Serialization: " + (d.getX() + d.getY() + d.getZ()));
+
+        try {
+
+            FileOutputStream fos = new FileOutputStream("dg.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(d);
+            
+            
+            oos.close();
+
+            FileInputStream fis = new FileInputStream("dg.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            d = (DungeonGame) ois.readObject();
+
+
+            ois.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+       System.out.println("After Serialization: " + (d.getX() + d.getY() + d.getZ()));
+         
+    }
+}
+
+class DungeonGame implements Serializable {
+    
+    public int x = 3;
+    transient long y = 4;
+    private short z = 5;
+
+
+    int getX() {
+        return x;
+    }
+
+    long getY() {
+        return y;
+    }
+
+    short getZ() {
+        return z;
+    }
+
+
+}
